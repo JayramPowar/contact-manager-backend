c0ba9e0f566c3444a04a491e41148340505ed62a
Jayram Powar <jayrampowar102@gmail.com>
2026-03-14 16:06:24 +0530

    integrate login functionality with JWT access token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,48 +1,73 @@
-import express from 'express';
-import bcrypt from 'bcrypt';
+import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
-import User from '../models/userModel.js';
-
+import User from "../models/userModel.js";
 
 export const registerUser = asyncHandler(async (req, res) => {
-    const {username, email, password} = req.body;
-    if(!username || !email || !password){
-        res.status(400);
-        throw new Error("All fields are mandatory");
-    }
-    //* Check if user already exists in database
-    const user = await User.findOne({email});
-    if(user){
-        res.status(400);
-        throw new Error("User already exists");
-    }
-    
-    //* Password hashing
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  //* Check if user already exists in database
+  const user = await User.findOne({ email });
+  if (user) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
 
-    //* Save hashed password in database as a password field
-    const newUser = await User.create({
-        username,
-        email,
-        password: hashedPassword,
-    });
-    if(newUser){
-        res.status(201).json({_id: newUser.id,
-            email: newUser.email
-        });
-    }else{
-        res.status(400);
-        throw new Error("User data is not valid");
-    }
-    
-    res.json({message :"Register here"});
+  //* Password hashing
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //* Save hashed password in database as a password field
+  const newUser = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  if (newUser) {
+    res.status(201).json({ _id: newUser.id, email: newUser.email });
+  } else {
+    res.status(400);
+    throw new Error("User data is not valid");
+  }
+
+  res.json({ message: "Register here" });
 });
 
 export const loginUser = asyncHandler(async (req, res) => {
-    res.json({message :"Login here"});
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+
+  //* Check if user already exists in database
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "15m" },
+    );
+    res.status(200).json({ accessToken });
+  }else{
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+
+  res.json({ message: "Login here" });
 });
 
 export const getCurrentUser = asyncHandler(async (req, res) => {
-    res.json({message :"Current user here"});
+  res.json({ message: "Current user here" });
 });
