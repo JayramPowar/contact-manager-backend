b92bc894ddfc0b7d37120744e5fb9532d3847859
Jayram Powar <jayrampowar102@gmail.com>
2026-04-04 16:34:15 +0530

    Making contact routes protected

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,15 +1,17 @@
 import express from "express";
-const routes = express.Router();
 import {getContacts, createContact, getContact, updateContact, deleteContact} from "../controllers/contactController.js";
+import validateTokenHandler from "../middlewares/validateTokenHandler.js";
+
+const routes = express.Router();
+
+//? middleware to validate token for all the routes in this file
+routes.use(validateTokenHandler)
 
 routes.route("/").get(getContacts);
-
 routes.route("/").post(createContact);
 
 routes.route("/:id").get(getContact);
-
 routes.route("/:id").put(updateContact);
-
 routes.route("/:id").delete(deleteContact);
 
 
